a2c5765f573c6a73bfff72168996d9e52482274d
Sato-Arisaa <arisugar1020@outlook.jp>
2024-09-22 11:36:13 +0900

    remove_button.dart

--- flutter_application_1/lib/infrastructure/remove_button.dart
+++ flutter_application_1/lib/infrastructure/remove_button.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> deleteStudent(String documentId) async {
+    try {
+      await FirebaseFirestore.instance.collection('students').doc(documentId).delete();
+      if (kDebugMode) {
+        print('Student with ID $documentId deleted successfully.');
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error deleting student: $e');
+      }
+    }
+}
